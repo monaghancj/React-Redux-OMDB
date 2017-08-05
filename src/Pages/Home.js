75625d00c85974f98e11a44761b737743bacd1ec
monaghancj <monaghancj@g.cofc.edu>
2017-08-04 21:25:21 -0400

    tachyons poster grid up

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -25,32 +25,40 @@ class App extends Component {
 
   }
   render() {
-    const buttonStyles = {height:'3rem', borderRadius: '2rem', margin:'.4rem .4rem 0 0'}
-    const poster = movie => `background: url(${movie.Poster}) no-repeat center center; background-size: cover;`
+    const buttonStyles = {height:'3rem', borderRadius: '2rem', margin:'.4rem .4rem 0 0', backgroundColor:'#255fbc'}
     return (    
       <div className="App">
         <Header />
-        <div className="mt4">
+        <div style={{marginTop:'5rem'}}>
+          <p className="tc black-80 f1 roboto mb0">Movie Search</p>
+          <p className="tc gray f3 roboto">OMDB API</p>
+        </div>
+
+        <div style={{marginTop:'10rem'}}>
           <div className="ba w-90 w-50-ns h3 center" style={{borderRadius:'2rem'}}>
             <div className="ml4">
               <input
-                className="dib ba b--white h2"
+                className="w-60 dib ba b--white h2"
                 style={{height:'3.75rem'}}
                 type="text"
                 placeholder="Search Movies"
                 onChange={e => this.onInputChange(e.target.value)}
                 value={this.state.movieTitle}
               />
-              <div className="dib bg-green v-mid fr w-33 pointer" style={buttonStyles} onClick={this.onSubmit}>
+              <div className="dib v-mid fr w-33 pointer" style={buttonStyles} onClick={this.onSubmit}>
                 <p className="tc white">Search</p>
               </div>
             </div>
           </div>
         </div>
-        <div className="w-70 center">
-          {map(movie => (
-            <img src={movie.Poster} key={movie.Title}/>
-          ), this.state.movies)}
+        <div className="mt4">
+          <article>
+            {map(movie => (
+              <a href={movie.Poster} className="fl w-50 w-25-l link overflow-hidden">
+                <div role="img" aria-label={movie.Title} className="grow aspect-ratio--4x6 " style={{background: 'url(' + movie.Poster + ') no-repeat center center', backgroundSize: 'cover'}} />
+              </a>
+            ), this.state.movies)}
+          </article>
         </div>
       </div>
     )
